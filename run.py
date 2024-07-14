d99251469a9fd60d9a1aaa7283f87a6ebd3f3575
Katarina Kilian <khkilian22@gmail.com>
2024-07-14 13:19:15 +0000

    added google worksheets, intial validate pin, added interupt keyboard

--- run.py
+++ run.py
@@ -5,17 +5,35 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('medication_inventory')
 
+WORKSHEETS = {
+    "nurse_pin": SHEET.worksheet("nurse_pin"),
+    "inventory": SHEET.worksheet("inventory"),
+    "patient_information": SHEET.worksheet("patient_information"),
+    "medication_administration_logs": SHEET.worksheet("medication_administration_logs"),
+    "guidelines": SHEET.worksheet("guidelines")
+}
+
+def validate_pin(entered_pin, worksheet):
+    """
+    Validation check of the pin to gain access into the system.
+    """
+    pin_data = worksheet.col_values(2)[1:]  
+    print(f"PIN data retrieved: {pin_data}")
+
+    if entered_pin in pin_data:
+        return True
+    return False   
 
 def get_login():
     """
-    Login required to enable system to start, and an added saftey measure for the BTM safe"
+    Login required to enable system to start, and an added safety measure for the BTM safe
     """
     print("Please enter the pin to start the program.")
     print("Enter the 4 pin number now\n")
@@ -23,6 +41,18 @@ def get_login():
 
     data_str = input("Enter your pin here: ")
 
-    print("You are now logged in")
+    if validate_pin(data_str, WORKSHEETS["nurse_pin"]):
+        print("You are now logged in")
+        return True
+    else:
+        print("Invalid pin entry, please try again .. \n")
+        return False
+        
 
-get_login()
+if __name__ == "__main__":
+    try:
+        get_login()
+    except KeyboardInterrupt:
+        # Handle the keyboard interrupt exception when the user presses Ctrl+C
+        print("\nApplication terminated by user. Quitting the application...")
+    
